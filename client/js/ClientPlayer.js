b7588f07e8aa8c9d1e8cc032896bdd310bdf6d09
jblanchette <jeff.blanchette5@gmail.com>
2019-09-02 22:35:54 -0400

    Add a player icon render step.  eventually this area will have more parts drawn to it.

--- client/js/ClientPlayer.js
+++ client/js/ClientPlayer.js
@@ -1,20 +1,102 @@
 
 const ClientPlayer = class {
-  constructor (playerId, startingPosition, units, playerColor) {
+  constructor (slot, playerId, startingPosition, units, displayName, race, playerColor) {
+    this.slot = slot;
     this.playerId = playerId;
     this.startingPosition = startingPosition;
-    this.units = units.map(unitData => new ClientUnit(unitData, playerColor));
+    this.displayName = displayName;
+    this.race = race;
+    this.playerColor = playerColor;
 
+    // make new ClientUnit instances
+    this.units = units.map(unitData => new ClientUnit(unitData, playerColor));
+    
     const sortedUnits = this.units.sort((a, b) => {
       return a.spawnTime - b.spawnTime;
     });
+
+    this.setup();
+  }
+
+  setup () {
+    const starterMap = {
+      'O': 'ogre',
+      'H': 'htow',
+      'E': 'etol',
+      'U': 'unpl'
+    };
+
+    const img = new Image();
+    const imgSrc = `/assets/wc3icons/${starterMap[this.race]}.jpg`;
+    
+    this.icon = null;
+
+    img.src = imgSrc;
+    img.onload = () => {
+      this.icon = img;
+    };
   }
 
   update (gameTime, dt) {
     this.units.forEach(unit => unit.update(gameTime, dt));
   }
 
+  renderPlayerIcon (ctx, gameTime, xScale, yScale, middleX, middleY) {
+    const iconSize = 45;
+    const halfIconSize = iconSize / 2;
+
+    const minXExtent = wc3v.xExtent[0];
+    const minYExtent = wc3v.yExtent[0];
+
+    const padding = 5;
+    const yMargin = 20;
+    const slotOffset = (this.slot * (iconSize + yMargin)) + halfIconSize;
+
+    const drawX = xScale(minXExtent) + middleX + halfIconSize + padding;
+    const drawY = yScale(minYExtent) + middleY + slotOffset + padding;
+
+    ctx.strokeStyle = "#FFFC01";
+    ctx.globalAlpha = this.decayLevel;
+
+    // clip our initial circle
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(drawX, drawY, halfIconSize, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+
+    // draw the icons
+    ctx.drawImage(
+      this.icon, 
+      (drawX - halfIconSize), 
+      (drawY - halfIconSize), 
+      iconSize, 
+      iconSize
+    );
+
+    // draw the icon ring
+    ctx.beginPath();
+    ctx.arc(drawX, drawY, halfIconSize, 0, Math.PI * 2, true);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.restore();
+
+    // adjust text by length, ensure we don't go out of the map bounds
+    const drawTextX = Math.max(
+      xScale(minXExtent) + middleX, 
+      (drawX - (this.displayName.length * 2))
+    );
+    const drawTextY = (drawY + halfIconSize + 10);
+
+    ctx.strokeText(this.displayName, drawTextX, drawTextY);
+
+    ctx.strokeStyle = "#000000";
+    ctx.globalAlpha = 1;
+  }
+
   render (ctx, gameTime, xScale, yScale, middleX, middleY) {
+    this.renderPlayerIcon(ctx, gameTime, xScale, yScale, middleX, middleY);
     this.units.forEach(unit => 
       unit.render(ctx, gameTime, xScale, yScale, middleX, middleY));
   }
