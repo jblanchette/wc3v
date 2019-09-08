b38c1195cec893c10d037a9ad49265fd2cdcc946
jblanchette <jeff.blanchette5@gmail.com>
2019-09-07 20:51:27 -0400

    unwind CSS scale and translate because its blurry.  keep track of transform object and use it.

--- client/js/gameData.js
+++ client/js/gameData.js
@@ -2,13 +2,18 @@ const gameData = {
   'maps': {
     '(2)concealedhill.w3x': {
       'name': 'ConcealedHill',
-      'xExtent': [-5200, 5200],
-      'yExtent': [6400, -6400]
+      'xExtent': [ -4500,  5600 ],
+      'yExtent': [  6400, -6400 ]
     },
     '(2)echoisles.w3x': {
       'name': 'EchoIsles',
-      'xExtent': [-7600, 7400],
-      'yExtent': [5100, -5700]
+      'xExtent': [ -7600,  7400 ],
+      'yExtent': [  5100, -5700 ]
+    },
+    '(4)synergybigpaved.w3x': {
+      'name': 'SynergyBigPaved',
+      'xExtent': [ -7424, 7424 ],
+      'yExtent': [ 6784, -6784 ]
     }
   }
 };
