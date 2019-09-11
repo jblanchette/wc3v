7d821675d09479b1a2007ddbd9fa183a08a69877
jblanchette <jeff.blanchette5@gmail.com>
2019-09-10 21:51:09 -0400

    not working zoom but checkign in as-is for debug

--- client/js/Drawing.js
+++ client/js/Drawing.js
@@ -26,6 +26,18 @@ const Drawing = class {
 
     ctx.restore();
   }
+
+  static rescaleX (x, transform) {
+    const range = x.range().map(transform.invertX, transform),
+        domain = range.map(x.invert, x);
+    return x.copy().domain(domain);
+  }
+
+  static rescaleY (y, transform) {
+    const range = y.range().map(transform.invertY, transform),
+        domain = range.map(y.invert, y);
+    return y.copy().domain(domain);
+  }
 };
 
 window.Drawing = Drawing;
