3f91334555a945d715923408181b672a3d39a856
jblanchette <jeff.blanchette5@gmail.com>
2019-09-25 22:45:13 -0400

    start work to add selected units tab.

--- client/js/Helpers.js
+++ client/js/Helpers.js
@@ -6,8 +6,54 @@ const distance = (pX, pY, qX, qY) => {
   );
 };
 
+////
+// check if two [itemId] lists are equal
+////
+
+const isEqualItemId = (itemIdA, itemIdB) => {
+  let isEqual = false;
+
+  if (itemIdA === null && itemIdB === null) {
+    // both null
+    return true;
+  } else if (itemIdA === null || itemIdB === null) {
+    // one is null and one isn't
+    return false;
+  }
+
+  // check to ensure each position in the list is equal
+  for (let i = 0; i < itemIdA.length; i++) {
+    if (itemIdA[i] !== itemIdB[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+////
+// helper to check if two given Unit's have equal itemId1 / itemId2 lists
+////
+
+const isEqualUnitItemId = (unitA, unitB) => {
+  if (!unitA || !unitB) {
+    return false;
+  }
+  
+  return isEqualItemId(unitA.itemId1, unitB.itemId1) &&
+         isEqualItemId(unitA.itemId2, unitB.itemId2);
+};
+
+const makeItemIdHash = (itemId1, itemId2) => {
+  return `[${itemId1.toString()}]-[${itemId2.toString()}]`;
+};
+
 const Helpers = {
-  distance: distance,
+  distance,
+  isEqualItemId,
+  isEqualUnitItemId,
+  makeItemIdHash,
 
   // constants
   MS_TO_SECONDS: 0.001,
