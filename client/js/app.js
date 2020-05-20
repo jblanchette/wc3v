f3f13a256c53b7c1347fbf66b4f538b0b36022e1
Jeff Blanchette <jeff.blanchette5@gmail.com>
2020-05-19 22:49:04 -0400

    correct path

--- client/js/app.js
+++ client/js/app.js
@@ -252,7 +252,7 @@ const Wc3vViewer = class {
     const { name } = this.mapInfo;
 
     return new Promise((resolve, reject) => {
-      this.loadFile(`http://${window.location.hostname}/maps/${name}/wpm.json`, (res) => {
+      this.loadFile(`../maps/${name}/wpm.json`, (res) => {
         const { target } = res;
         const jsonData = JSON.parse(target.responseText);
           
@@ -268,7 +268,7 @@ const Wc3vViewer = class {
     const { name } = this.mapInfo;
 
     return new Promise((resolve, reject) => {
-      this.loadFile(`http://${window.location.hostname}/maps/${name}/doo.json`, (res) => {
+      this.loadFile(`../maps/${name}/doo.json`, (res) => {
         const { target } = res;
         const jsonData = JSON.parse(target.responseText);
           
